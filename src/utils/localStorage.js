99d6eb336992fcdc84d31461232ae80bde08d4a0
Anujah <anujapanchariya7@gmail.com>
2026-01-03 20:00:50 +0530

    adding css ,login

---
+++ src/utils/localStorage.js
@@ -0,0 +1,10 @@
+export const getRestaurants =() =>{
+
+    return
+    JSON.parse(localStorage.getItem("evalData")) ||[];
+};
+
+export const saveRestaurants =(data)=>{
+    localStorage.setItem("evalData",JSON.stringify(data));
+
+};
